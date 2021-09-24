fa187d394b7c417768a43b536508468de5ed1f0b
Rafael Tavares <rafatavares03@hotmail.com>
2021-09-23 21:46:36 -0300

    mobile menu compleated (now it's true :P (I hope :') )

--- blogr-landing-page-main/assets/js/script.js
+++ blogr-landing-page-main/assets/js/script.js
@@ -6,11 +6,12 @@ document.querySelector('#menu-button').addEventListener('click', (e) => {
 
 document.querySelector('#menu').addEventListener('click', (e) => {
     const menuButton = document.querySelector('#menu-button');
-
+    
     if (e.target === document.querySelector('.mobile-menu.isOpen')) {
         menuButton.classList.replace('close-menu', 'open-menu');
-
+        
         e.target.classList.remove('isOpen');
+        CloseDropdown();
     }
 });
 
